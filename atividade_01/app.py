753b874b97bfc962225016cd68266b39cfbb932f
PAULA GONÇALVES BATISTA <paula.batista@e239.local>
2026-02-26 09:52:08 -0300

    atividade 01

--- atividade_01/app.py
+++ atividade_01/app.py
@@ -1,50 +1,48 @@
-# importando o flask para a aplicação.
-
 from flask import Flask, render_template
 
-# Carregando o Flask na variavel "APP".
-# Declarando variavel no Python.
+app = Flask(__name__, template_folder='Views')
 
-app = Flask(__name__, template_folder ='Views')
-
-# Variaveis com  _ são variáveis de ambiente no Python.
-# (__name__) representa o nome da aplicação.
-
-# Criando a rota pricipal do site.
-
+# Rota principal
 @app.route('/')
-
-
-#DEF cria funções no python
-
 def home():
-    return render_template('/index.html')
+    return render_template('index.html')
 
 @app.route('/personagem')
-
 def personagem():
     return render_template('personagem.html')
 
 @app.route('/sagas')
-
 def sagas():
     return render_template('sagas.html')
 
 @app.route('/formulario')
-
 def formulario():
     return render_template('formulario.html')
 
-
-
 @app.route('/noticias')
-
 def noticias():
     return render_template('noticias.html')
-#Iniciando o servidor na porta 5000.
-#O metodo .run() inicia o servidor.
-#Esta verificando se o arquivo gravado em __name__ é o arquivo principal.
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='Views')
+
+@app.route('/formulario', methods=['GET','POST'])
+def formulario():
+    personagem = None
+
+    if request.method == 'POST':
+        personagem = {
+            "nome": request.form['nome'],
+            "armadura": request.form['armadura'],
+            "descricao": request.form['descricao']
+        }
+
+    return render_template('formulario.html', personagem=personagem)
+#Iniciando o servidor na porta 5000.
+#O metodo .run() inicia o servidor.
+#Esta verificando se o arquivo gravado em __name__ é o arquivo principal. 
 
