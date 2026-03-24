720055e87ba67da32ec060ff798b950b5216dc24
PAULA GONÇALVES BATISTA <paula.batista@e239.local>
2026-03-24 10:49:02 -0300

    arquitetura de mvc e templates

--- aula-03-arquitetura-mvc-e-request/controllers/routes.py
+++ aula-03-arquitetura-mvc-e-request/controllers/routes.py
@@ -7,9 +7,10 @@ app = Flask(__name__, template_folder='views')
 
 def init_app(app):
 
-    listaConsoles = ['Playstation 5', 'Xbox One','Super Nintendo', 'Atari 2600', 'Nintendo 3DS']
-    listaGames =[{'titulo': 'CS-GO', 'ano':2012,'categoria': 'FPS-ONLINE', 'plataforma': 'PC(WINDOWS)'}]
-
+    listaConsoles = ['Playstation 5', 'Xbox One',
+            'Super Nintendo', 'Atari 2600', 'Nintendo 3DS']
+    listaGames = [{'titulo' : 'CS-GO', 'ano' : 2012, 'categoria' : 'FPS Online', 'plataforma' : 'PC (Windows)'}]
+    
     @app.route('/')
     def home():
         return render_template('index.html')
@@ -28,21 +29,15 @@ def init_app(app):
         # criando um objeto
         console = {"Nome:": "Playstation 2 ",
                    "Fabricante: ": "Sony", "Ano: ": 2000}
-        #Recebendo o valor do formulário
+        # Recebendo o valor do formulário
         if request.method == 'POST':
             if request.form.get('novoConsole'):
                 listaConsoles.append(request.form.get('novoConsole'))
-        
+
         return render_template('consoles.html', console=console, listaConsoles=listaConsoles)
-     #Recebendo as informações de cadastro de jogos
-     
+
     @app.route('/cadgames', methods=['GET', 'POST'])
     def cadgames():
-       
-       #Recebendo os dados do formulario e devolvendo para a pagina
-       #Verificando se a requisição é do tipo POST
-      if request.method == 'POST':
-         #Aqui ele ira gravar os dados na lista de jogos 
-        listaGames.append({'titulo' : request.form.get('titulo'), 'ano' : request.form.get('ano'), 'categoria' : request.form.get('categoria'),  'plataforma' : request.form.get('plataforma')})
-      
-        return render_template('cadgames.html', listaGames=listaGames)
+        if request.method == 'POST':
+            listaGames.append({'titulo' : request.form.get('titulo'), 'ano' : request.form.get('ano'), 'categoria' : request.form.get('categoria'), 'plataforma' : request.form.get('plataforma')})
+        return render_template('cadgames.html', listaGames = listaGames)
