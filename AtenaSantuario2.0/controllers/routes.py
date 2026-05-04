5ca4eb16af13b901e7082647dbe635f161ead947
PAULA GONÇALVES BATISTA <paula.batista@e239.local>
2026-05-04 14:25:04 -0300

    aulas

--- AtenaSantuario2.0/controllers/routes.py
+++ AtenaSantuario2.0/controllers/routes.py
@@ -33,25 +33,52 @@ def init_app(app):
             "descricao": "Novo arco de Cavaleiros do Zodíaco chega em maio.",
             "imagem": "sagadoceu.png",
             "link": "https://ovicio.com.br/os-cavaleiros-do-zodiaco-saga-do-ceu-tem-lancamento-confirmado-para-maio/"
-        },
-        {
-            "id": 2,
-            "titulo": "Novo anime anunciado",
-            "descricao": "Nova adaptação confirmada oficialmente.",
-            "imagem": "anime.png",
-            "link": "https://olhardigital.com.br/2026/03/06/curiosidades/saint-seiya-prepara-nova-animacao-que-pode-finalmente-adaptar-a-saga-de-zeus-apos-decadas-de-espera/"
-        },
-        {
-            "id": 3,
-            "titulo": "Mangá continua",
-            "descricao": "Histórias inéditas seguem no Japão.",
-            "imagem": "manga.png",
-            "link": "https://www.cavzodiaco.com.br/noticia/23/02/2026/novo-manga-masami-kurumada-mostra-primeiros-esbocos-da-saga-do-ceu-dos-cavaleiros-do-zodiaco"
         }
     ]
-
+    listaFilmes = [
+        {
+            "titulo": "O Começo",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-saint-seiya-o-comeco-t319748/"
+        },
+        {
+            "titulo": "A Lenda do Santuário",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-a-lenda-do-santuario-t30816/"
+        },
+        {
+            "titulo": "Teatro Piriri (Apresentação)",
+            "link": "https://www.youtube.com/watch?v=w_CFFlAZXtw&t=815s"
+        },
+        {
+            "titulo": "O Santo Guerreiro",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-1-o-santo-guerreiro-t1363/assista-agora/"
+        },
+        {
+            "titulo": "A Grande Batalha dos Deuses",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-2-a-grande-batalha-dos-deuses-t888/"
+        },
+        {
+            "titulo": "A Lenda dos Defensores de Atena",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-3-a-lenda-dos-defensores-de-atena-t1361/"
+        },
+        {
+            "titulo": "Os Guerreiros do Armagedon",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-4-os-guerreiros-do-armagedon-t8704/"
+        },
+        {
+            "titulo": "Episódio Zero",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-episodio-zero-t121496/"
+        },
+        {
+            "titulo": "O Mito dos Cavaleiros Renegados",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-o-mito-dos-cavaleiros-renegados-t40650/"
+        },
+        {
+            "titulo": "Prólogo do Céu",
+            "link": "https://filmow.com/os-cavaleiros-do-zodiaco-o-filme-prologo-do-ceu-t1362/"
+        }
+    ]
     # =========================
-    # 🛡️ API CAVALEIROS (JSON LOCAL)
+    # 🛡️ API JSON
     # =========================
     cavaleirosjson = [
         {
@@ -60,37 +87,30 @@ def init_app(app):
             "armadura": "Pégaso",
             "signo": "Sagitário",
             "imagem": "/static/imgs/seiya.png"
-        },
-        {
-            "id": 2,
-            "nome": "Shiryu",
-            "armadura": "Dragão",
-            "signo": "Libra",
-            "imagem": "/static/imgs/shiryu.png"
         }
     ]
 
     # =========================
-    # 📚 LINKS DOS MANGÁS (DRIVE)
+    # 📚 MANGÁS (DRIVE)
     # =========================
     mangas_links = {
         "classico": "https://drive.google.com/drive/folders/1PGbkWHPkEt6U6SxmCkw8-_tmvFmJBcq5",
         "saintia": "https://drive.google.com/drive/folders/1B6ystPNV1U2xV56YrrRxh7hxwTCaefr6",
-        "rerise": "https://drive.google.com/drive/folders/1HbXzL7reslzYwHK8WOOZ0BwM5U99FR7R",
-        "omega": "https://drive.google.com/drive/folders/1YTBkoYv4YKGl77A5xsTsF3uYpBUp6W-I",
-        "novels": "https://drive.google.com/drive/folders/1yWo93gq0bN-H6ryxbC0VfLQLmMnjeEjR",
-        "next": "https://drive.google.com/drive/folders/1byOCHAatBZ5LQzldrLgvXMwrKQ4d6Am6",
-        "gaiden": "https://drive.google.com/drive/folders/1iy8C4pxGbVb6rcZDlrlv_jbd-mvnxiE9",
-        "lost": "https://drive.google.com/drive/folders/1G0OtRCDqk98Sbuh7Y_cHo02IwrUfsv7f",
-        "epg_assassin": "https://drive.google.com/drive/folders/1AnCbfAPs4nRo_qOH8OXgXR--jQuX9o3O",
-        "epg": "https://drive.google.com/drive/folders/1WLp9TjmSilUWyfZOTVcZ9MWhDDJy3Ryc",
-        "ep0": "https://drive.google.com/drive/folders/1iU3Q_LMJd8JBXK5wG4RMPzfLjWoQvQBj",
-        "enciclopedias": "https://drive.google.com/drive/folders/1OPnLonSk_-UGvDxowJ4nB3duVPcHSWDb",
-        "dark": "https://drive.google.com/drive/folders/1-riR33Tw_KdPKNVNsf4D1PQKBPqHYs_0",
-        "origin": "https://drive.google.com/drive/folders/16gT0DRZFrW67k9gIqFXXbgaqRbzfvIuE",
-        "destiny": "https://drive.google.com/drive/folders/1Pms-ySV2RK--uBdWNQCl_D3bSZH0vyhF"
+        "rerise": "https://drive.google.com/drive/folders/1HbXzL7reslzYwHK8WOOZ0BwM5U99FR7R"
     }
 
+    # =========================
+    # 🎬 ANIMES (ASSISTIR)
+    # =========================
+    animes_links = {
+        "omega": "https://animesonlineclub.net/anime/saint-seiya-omega-os-cavaleiros-do-zodiaco-omega",
+        "lost_canvas": "https://animesonlineclub.net/anime/saint-seiya-the-lost-canvas",
+        "soul_gold": "https://animesonlineclub.net/anime/saint-seiya-soul-of-gold",
+        "saintia": "https://animesonlineclub.net/anime/saint-seiya-saintia-shou",
+        "classico": "https://animesonlineclub.net/anime/os-cavaleiros-do-zodiaco",
+        "reboot": "https://animesonlineclub.net/anime/saint-seiya-os-cavaleiros-do-zodiaco"
+    }
+    
     # =========================
     # 🏠 HOME
     # =========================
@@ -148,11 +168,25 @@ def init_app(app):
         return render_template('noticias.html', listaNoticias=listaNoticias)
 
     # =========================
-    # 📚 MANGÁS (TODOS LINKS)
+    # 📚 MANGÁS
     # =========================
     @app.route('/mangas')
     def mangas():
         return render_template('mangas.html', mangas=mangas_links)
+
+    # =========================
+    # 🎬 ASSISTIR ANIMES
+    # =========================
+    @app.route('/assistir')
+    def assistir():
+        return render_template('assistir.html', animes=animes_links)
+    
+# =========================
+# 🎬 FILMES CDZ
+# =========================
+    @app.route('/filmes')
+    def filmes():
+        return render_template('filmes.html', listaFilmes=listaFilmes)
 
     # =========================
     # 🔥 API CAVALEIROS
